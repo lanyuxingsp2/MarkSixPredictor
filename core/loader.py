05643217e2ce3ab8283c3127eb220289b5d8099d
yhx5354 <haitan01@hotmail.com>
2025-12-14 00:15:47 +0800

    Update loader.py

--- core/loader.py
+++ core/loader.py
@@ -2,11 +2,6 @@ import pandas as pd
 
 def load_history(path):
     df = pd.read_csv(path, encoding="utf-8")
-    required_cols = ["年份", "期号", "号1", "号2", "号3", "号4", "号5", "号6"]
-
-    for col in required_cols:
-        if col not in df.columns:
-            raise Exception(f"缺少字段：{col}")
-
-    return df
-# loader.py 读取 CSV
+    cols = ["年份","期号","号1","号2","号3","号4","号5","号6"]
+    df = df[cols]
+    return df.astype(int)
