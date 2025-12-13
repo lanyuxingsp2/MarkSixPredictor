5a419d42a1af27a1b4e46bec61c2bc903177a70a
yhx5354 <haitan01@hotmail.com>
2025-12-14 00:15:01 +0800

    Update analyzer.py

--- core/analyzer.py
+++ core/analyzer.py
@@ -1,1 +1,8 @@
+from collections import Counter
+
+def count_frequency(df):
+    nums = []
+    for _, row in df.iterrows():
+        nums.extend([row[i] for i in ["号1","号2","号3","号4","号5","号6"]])
+    return dict(Counter(nums))
 
