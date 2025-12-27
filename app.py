2e2034f676c60a697c2c86a582512bc7930d1880
lanyuxingsp2 <34023515+lanyuxingsp2@users.noreply.github.com>
2025-12-28 01:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
 
+from PyQt5.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
